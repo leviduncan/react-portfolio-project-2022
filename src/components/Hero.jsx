a5e617f026af7ff9cc9a0b2604f90233a686a05c
Darrin Duncan <darrin@darrinduncan.com>
2022-04-14 18:30:58 -0400

    Update Rez link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,5 @@
+import rez from '../assets/DARRIN-DUNCAN-FED-REZ.pdf'
+
 function Hero() {
     return (
         <header id="top" className="hero d-flex align-items-center">
@@ -9,7 +11,7 @@ function Hero() {
                         <h2 className="my-3">that help businesses solve problems</h2>
                         <div className="d-flex flex-column flex-md-row">
                             <div className="">
-                                <a href="https://darrin-duncan.com/static/media/darrin_duncan_resume.6684cecb0d01db0401fe.pdf" className="btn btn-primary btn-lg btn-block">Resume</a>
+                                <a href={rez} className="btn btn-primary btn-lg btn-block" target="_blank">Resume</a>
                             </div>
                         </div>
                     </div>
