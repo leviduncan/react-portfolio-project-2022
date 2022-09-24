6e5a08fb19d580654380a1d2fbae7e675f632955
darrin@darrinduncan.com <darrin@darrinduncan.com>
2022-09-24 19:40:37 -0400

    Layout update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,20 +2,24 @@ import rez from '../assets/DARRIN-DUNCAN-FED-REZ.pdf'
 
 function Hero() {
     return (
-        <header id="top" className="hero d-flex align-items-center flex-lg-row flex-column">
-            <div class="col-lg-7 col-sm-12 left-content d-flex align-items-center">
-                <div class="container">
-                    <h3>My Name is Darrin,</h3>
-                    <h1>I am a Front-End Web Developer</h1>
-                    <div className="d-flex flex-column flex-md-row">
-                        <div className="">
-                            <a href={rez} className="btn btn-primary btn-lg btn-block" target="_blank" rel="noreferrer">Resume</a>
+        <header id="top" className="hero">
+            <div className="container d-flex  flex-column flex-lg-row">
+                <div class="col-lg-7 col-sm-12 left-content d-flex align-items-center">
+                    <div className="">
+                        <h3>My Name is Darrin,</h3>
+                        <h1>I build engaging front-end experiences</h1>
+                        <div className="d-flex flex-column flex-md-row">
+                            <div className="">
+                                <a href={rez} className="btn btn-primary btn-lg btn-block" target="_blank" rel="noreferrer">Resume</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-lg-5 col-sm-12 right-content">
-                <h2 className="">I use code to help solve business challenges</h2>
+                <div className="col-lg-5 col-sm-12 d-flex justify-content-center align-items-center">
+                    <div className="right-content">
+                        <h2 className="sub">I use code to help solve business challenges</h2>
+                    </div>
+                </div>
             </div>
         </header>
     )
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,7 +5,7 @@ function Menu() {
         
             <nav className="navbar navbar-expand-lg navbar-light fixed-top">
             <div className="container">
-                <a className="navbar-brand" href="/">Darrin Duncan</a>
+                <a className="navbar-brand" href="/"><div className="logo">D</div></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     {/* <span className="navbar-toggler-icon"></span> */}
                     <FaHamburger />
@@ -14,21 +14,23 @@ function Menu() {
                 <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul className="navbar-nav d-flex align-items-center justify-content-between">
                         <li className="nav-item">
+                            <a className="nav-link" href="#top">Home</a>
+                        </li>
+                        <li className="nav-item">
                             <a className="nav-link" href="#projects">Projects</a>
                         </li>
                         <li className="nav-item">
                             <a className="nav-link" href="#skills">Skills</a>
                         </li>
                         <li className="nav-item">
-                           
-                            <a className="nav-link" href="#about">About<span className="sr-only">(current)</span></a>
+                            <a className="nav-link" href="#about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#connect">Connect</a>
                         </li>
                         {/* <li className="nav-item">
                             <a className="nav-link" href="#coding">Code Challenge</a>
                         </li> */}
-                        <li className="nav-item">
-                            <a href="#connect" className="btn btn-org btn-sm">Connect</a>
-                        </li>
                     </ul> 
                 </div>
             </div>
